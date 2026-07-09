97f1d29f5cf1f39723117681b212a2a191e07fff
Siranjeevan <smartjeevith2k@gmail.com>
2026-07-08 23:48:48 -0700

    Initial Commit

---
+++ TimeZoneSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeZoneSwitcher
+//
+//  Created by Jeevith on 7/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeZoneSwitcher/TimeZoneSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeZoneSwitcherApp.swift
+//  TimeZoneSwitcher
+//
+//  Created by Jeevith on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct TimeZoneSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
